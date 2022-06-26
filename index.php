feb255b0cde0c91fc45817b5968762d038753b54
Arianto <97211033@student.itk.ac.id>
2022-06-26 23:27:13 +0800

    styling with bootsrap

--- index.php
+++ index.php
@@ -5,14 +5,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Buku</title>
+    <!-- link bootstrap css -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 
 <body>
+    <!-- header -->
+    <nav class="navbar navbar-dark bg-primary ">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQtDnceY0jACTVFGCiLaKcr9xSuL7LaeE6dQ&usqp=CAU" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                Koleksi Buku
+            </a>
+        </div>
+    </nav>
+    <!-- end header -->
+
+    <!-- table -->
+    <div class="container">
+        <table class="table table-bordered text-center mt-2">
+            <thead class="table-primary">
+                <tr>
+                    <th>ID</th>
+                    <th>Judul</th>
+                    <th>Pengarang</th>
+                    <th>Tahun</th>
+                    <th>Kategori</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Pemrograman Web</td>
+                    <td>Didik Setiawan</td>
+                    <td>2020</td>
+                    <td>Pendidikan</td>
+                    <td>Hapus | Edit</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
 
-
+    <!-- script bootstrap js -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 
